7ee22e7d5f61d8cdd0b2d95b0ec88e9b68db4045
David Ramos <ramos_drh@hotmail.com>
2020-04-25 19:08:44 +0200

    Añade atributos en clase modelo

---
+++ src/main/java/daw/david/tarea4davidramos/Pelota.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw.david.tarea4davidramos;
+
+/**
+ *
+ * @author ramos
+ */
+public class Pelota {
+    private double peso;
+    private String material;
+    private String deporte;
+}
